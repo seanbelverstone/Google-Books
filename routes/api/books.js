c9cf057c453d145a6de09ecd6e9556a071bdf91a
Sean Belverstone <seanbelverstone@gmail.com>
2019-10-11 15:54:22 -0500

    Created the book model

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,14 +1,16 @@
 const router = require("express").Router();
 const booksController = require("../../controllers/booksController");
 
-// Matches with "/api/books"
 router.route("/")
-  .get(booksController.findAll);
+// Gets all the books
+  .get(booksController.findAll)
+//Saves a book into the database
+  .post(booksController.create);
 
 // Matches with "/api/books/:id"
 router
   .route("/:id")
-  .get(booksController.findById)
+//Remove a book from saved
   .delete(booksController.remove);
 
 module.exports = router;
