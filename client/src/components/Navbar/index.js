9561d381abc4534b099f4eebd8fdf914228ad3e9
Sean Belverstone <seanbelverstone@gmail.com>
2019-10-11 16:12:23 -0500

    Created a navbar and implemented the use of react-router-dom. This enables me to have routing within the page.

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Link } from "react-router-dom";
+
+const MainNav = (props) => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+  return (
+    <div>
+      <Navbar color="faded" light>
+        <NavbarBrand href="/" className="mr-auto">Google Books</NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar>
+            <NavItem>
+              <Link to="/">Search</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/saved">Saved</Link>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default MainNav;
