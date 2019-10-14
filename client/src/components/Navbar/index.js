d68fb2bbff5f02cfba88a24f042dfb938fd9d9ed
Sean Belverstone <seanbelverstone@gmail.com>
2019-10-13 20:20:37 -0500

    Had some compiling issues. Had installed dependencies in the wrong area.

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem } from 'reactstrap';
 import { Link } from "react-router-dom";
 
 const MainNav = (props) => {
