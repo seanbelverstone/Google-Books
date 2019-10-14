78cb5a334d67e1e3f36b4b6e12c68e8ac285d5cd
Sean Belverstone <seanbelverstone@gmail.com>
2019-10-14 18:21:28 -0500

    Created the file structure and made an unordered list, containing the book component.

---
+++ client/src/components/List/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Book from "../Book";
+
+const List = () => {
+    return(
+        <div>
+            <ul>
+                <Book></Book>
+            </ul>
+        </div>
+    )
+}
+
+export default List;
