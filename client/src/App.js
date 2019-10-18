000a2946ead0433d619603c4674165e6d10fb976
Sean Belverstone <seanbelverstone@gmail.com>
2019-10-17 21:09:26 -0500

    Had to do some shuffling but now managed to get my Router to work properly. Realised Id basically put all my code on App.js instead of having separate pages, in order to flick between the two! Might have been better to nest them in a "pages" directory, but Ive just done it in a different component. Much smoother now :)

--- client/src/App.js
+++ client/src/App.js
@@ -1,87 +1,35 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import MainNav from "./components/Navbar";
 import MainJumbotron from "./components/MainJumbotron";
-import SearchBar from "./components/SearchBar";
-import List from "./components/List";
-import Book from "./components/Book";
 import SavedBooks from "./components/SavedBooks";
-import API from "./utils/API";
+import SearchPage from "./components/SearchPage";
 import "./App.css";
 
 class App extends Component {
-
-  state = {
-    search: "",
-    bookResults: []
-  };
-
-  handleChange = (event) => {
-    const {name, value} = event.target;
-    this.setState(
-      {[name]: value}
-    )
-  }
-
-  getSearch = (event) => {
-    event.preventDefault();
-
-    // Using utils, makes the axios call
-    API.getSearch(this.state.search).then(results => {
-
-      // Sets the state of bookResults to the API results
-      this.setState(
-        {bookResults: results.data.items});
-    });
-    this.setState(
-      {search: ""});
-  }
-
-  saveBook = (book) => {
-    let requestBody = {
-      "bookId": book.id, 
-      "title": book.volumeInfo.title, 
-      "authors": book.volumeInfo.authors,
-      "description": book.volumeInfo.description,
-      "image": book.volumeInfo.imageLinks.thumbnail,
-      "link": book.volumeInfo.canonicalVolumeLink
-    }
-    console.log(requestBody);
-
-    API.saveBook(requestBody)
-    .then(() => {
-      console.log("Done")
-    });
-  }
 
   render() {
     return (
       <Router>
         <div className="container">
           <MainNav>
-            <Route exact path="/" />
+
+            <Route path="/" component={SearchPage}/>
             <Route path="/saved" component={SavedBooks} />
+
           </MainNav>
           <MainJumbotron />
-          <SearchBar 
-          search={this.state.search}
-          onChange={this.handleChange}
-          onClick={this.getSearch}/>
-        <List>
-          {this.state.bookResults.map(book => {
-            return(
-              <Book 
-                key={book.id}
-                title={book.volumeInfo.title}
-                image={book.volumeInfo.imageLinks.thumbnail}
-                author={book.volumeInfo.authors}
-                synopsis={book.volumeInfo.description}
-                purchaseLink={book.volumeInfo.canonicalVolumeLink}
-                onClick={() => this.saveBook(book)}/>
-            )
-          })}
-        </List>
-        <SavedBooks />
+
+          <Switch>
+          <Route path="/saved">
+            <SavedBooks />
+          </Route>
+          <Route path="/">
+            <SearchPage />
+          </Route>
+        </Switch>
+
+
         </div>
       </Router>
     );
