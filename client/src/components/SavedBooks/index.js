230f146627522334e8b875a15ee16911c7058688
Sean Belverstone <seanbelverstone@gmail.com>
2019-10-17 22:01:07 -0500

    Tried to get the books to load from a componentDidMount function but nothing is happening

--- client/src/components/SavedBooks/index.js
+++ client/src/components/SavedBooks/index.js
@@ -1,10 +1,40 @@
 import React, { Component } from "react";
+import List from "../List";
+import Book from "../Book";
+import API from "../../utils/API";
 
 class SavedBooks extends Component {
+
+    state = {
+        books: []
+    }
+
+    componentDidMount() {
+        this.loadBooks();
+    }
+
+    loadBooks = () => {
+        API.getBooks()
+        .then(res => 
+            this.setState({savedBooks: res.data})
+            )
+            .catch(err => console.log(err));
+    }
+
     render() {
         return(
-            <div>
-                <h1>This is where the saved books go.</h1>
+            <div className="bookDisplay">
+                <List>
+                    {this.state.books.map(book => (
+                        <Book
+                            key={book.id}
+                            title={book.volumeInfo.title}
+                            image={book.volumeInfo.imageLinks.thumbnail}
+                            author={book.volumeInfo.authors}
+                            synopsis={book.volumeInfo.description}
+                            purchaseLink={book.volumeInfo.canonicalVolumeLink} />
+                    ))}
+                </List>
             </div>
         )
     }
